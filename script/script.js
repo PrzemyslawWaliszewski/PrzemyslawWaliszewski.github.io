1161f2042dd39bec17eef5cec89a9947e493d803
PrzemyslawWaliszewski <PrzemyslawWaliszewski@tieto.com>
2019-06-18 23:42:09 +0200

    v1.0

--- script/script.js
+++ script/script.js
@@ -1,7 +1,29 @@
-document.querySelector('.navbar-toggle').addEventListener('click', () => {
-   const navMenu = document.querySelector('.navbar-menu');
+//navbar-nav
+function toggleMenu(){
+    var menu = document.getElementById('menu');
+    console.log(menu);
+    menu.classList.toggle('active');
+}
+window.onscroll = function changeNav(){
+    var scroll =  window.scrollY;
+    NAV = document.getElementById('mainNav');
+    NAVLink = document.getElementsByClassName('nav-link');
+    NAVBAR = document.getElementsByClassName('navbar-brand');
+    //console.log(NAVBAR);
+    if (scroll >= 500) {
+        NAV.classList.add("BGWhite");
+        var i;
+        for(i=0; i < NAVLink.length;i++){
+            NAVLink.item(i).classList.add("FontBL");
+        }
+        NAVBAR.item(0).classList.add("FontBL");
+    } else {
+        NAV.classList.remove("BGWhite");
+        var i;
+        for(i=0; i < NAVLink.length;i++){
+            NAVLink.item(i).classList.remove("FontBL");
+        }
+        NAVBAR.item(0).classList.remove("FontBL");
+    }
+}
 
-   navMenu.classList.contains('hidden') ?
-       navMenu.classList.remove('hidden') :
-       navMenu.classList.add('hidden');
-});
